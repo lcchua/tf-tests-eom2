803a59f229d0846a03774f15d0767ea1bffe9fb1
lcchua <laichwang@gmail.com>
2024-09-23 16:30:42 +0800

    Added comments to ws_install.sh

--- ws_install.sh
+++ ws_install.sh
@@ -10,5 +10,6 @@ chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
+# curl http://localhost:80
 echo "<h1>Hello World! $(hostname -f)</h1>" > /var/www/html/index.html
 reboot
