af720cc381e3f81b4a886ea41ea8d0f8e5c3d4b5
lcchua <laichwang@gmail.com>
2024-09-23 19:34:44 +0800

    added comments to ws_install sh

--- ws_install.sh
+++ ws_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# After systemctl start httpd, can see the Apache test page 
+# if you enter the EC2 Public IPv4 address in your Internet Browser
+# Or else after the EC2 instance is ready, can curl http://localhost:80
+# to see the display message below
+
 yum update -y
 yum install httpd -y
 # yum install docker -y
@@ -10,6 +15,5 @@ chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
-# curl http://localhost:80
 echo "<h1>Hello World! $(hostname -f)</h1>" > /var/www/html/index.html
 reboot
